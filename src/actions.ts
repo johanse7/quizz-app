47e11fa1777f643ab90c08325885521e09db1d80
Jhoan Sebastian <johanrodriguez_14@hotmail.com>
2024-12-17 18:01:02 -0500

    update path data.js

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,13 @@
 "use server";
 
 import { readFile } from "fs/promises";
+import path from "path";
 import { Question, Quiz } from "./types";
 
 export const getQuizzes = async (): Promise<Array<Quiz> | undefined> => {
-  const result = await readFile("data.json", { encoding: "utf8" });
+  const result = await readFile(path.resolve("data.json"), {
+    encoding: "utf8",
+  });
   const data = result ? JSON.parse(result) : [];
   return data?.quizzes?.map(({ title, icon }: Quiz) => ({ title, icon }));
 };
@@ -12,7 +15,9 @@ export const getQuizzes = async (): Promise<Array<Quiz> | undefined> => {
 export const getQuestionByQuiz = async (
   quizName: string
 ): Promise<Array<Question> | undefined> => {
-  const result = await readFile("data.json", { encoding: "utf8" });
+  const result = await readFile(path.resolve("data.json"), {
+    encoding: "utf8",
+  });
   const data = result ? JSON.parse(result) : [];
   const quiz: Quiz & { questions: Array<Question> } = data?.quizzes?.find(
     ({ title }: Quiz) => title === quizName
@@ -21,7 +26,9 @@ export const getQuestionByQuiz = async (
 };
 
 export const getQuizByName = async (name: string) => {
-  const result = await readFile("data.json", { encoding: "utf8" });
+  const result = await readFile(path.resolve("data.json"), {
+    encoding: "utf8",
+  });
   const data = result ? JSON.parse(result) : [];
   const quiz: Quiz = data?.quizzes?.find(({ title }: Quiz) => title === name);
   return quiz;
